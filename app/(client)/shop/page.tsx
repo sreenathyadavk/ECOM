a50639d52e660c4dbb9d88b71214475e70b4490d
Sreenath Yadav <sreenathyadavk@gmail.com>
2026-06-30 07:20:21 +0530

    Wrap shop page in Suspense boundary

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,13 +1,15 @@
 import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
+import React, { Suspense } from "react";
 
 const ShopPage = async () => {
   const categories = await getCategories();
   const brands = await getAllBrands();
   return (
     <div className="bg-white">
-      <Shop categories={categories} brands={brands} />
+      <Suspense fallback={<div className="flex justify-center p-20">Loading shop...</div>}>
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
 };
