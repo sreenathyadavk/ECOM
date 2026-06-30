69f1c8dee39a7144eb25e3c4b276e74545765623
Sreenath Yadav <sreenathyadavk@gmail.com>
2026-06-30 07:38:55 +0530

    Configure default Clerk keys in next.config.ts for runtime rendering environment

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_c2VsZWN0ZWQtZ2xvd3dvcm0tNzIuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || "sk_test_dummyclerksecretkey1234567890",
+  }
 };
 
 export default nextConfig;
